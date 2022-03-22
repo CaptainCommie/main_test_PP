cf34cccb97fa44504b2260f3ede411d6046a4211
CaptainCommie <93552955+CaptainCommie@users.noreply.github.com>
2022-03-22 22:20:45 +0000

    Fixed Double jump and cleaned up code slightly

--- testing/Assets/Scripts/CharacterMovement.cs
+++ testing/Assets/Scripts/CharacterMovement.cs
@@ -6,75 +6,75 @@ public class CharacterMovement : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D body;
-    private bool grounded;      //checks whether the character is grounded
+    private BoxCollider2D box_collider;
+    [SerializeField] private LayerMask ground_layer;
     [SerializeField] private float speed;       //this affects the speed of vertical movement
     [SerializeField] private float jump_power;      //this affects the height of the jump
-    [SerializeField] private float max_jumps = 2;       //this is how many jumps the character can do
-    private int jump_amount = 0;        //This relates to my double jumping mechanic
+    [SerializeField] private int extra_jumps_count;      //this is how many extra jumps a player has
     
-
-
-    //Awake is called when the script is initially run
-    private void Awake() {
+    private int extra_jumps;
+    
+    private void Awake() 
+    {
         //grab references for rigidboy and animator components from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        box_collider = GetComponent<BoxCollider2D>();
     }
 
-    
-    private void Update() {
+    private void Update() 
+    {
         //this stores the value of the horizontal movement
         float horizontal_input = Input.GetAxis("Horizontal");
-        //This is the code for horizontal movement and does not affect vertical movement
+        //This moves the player is a horizontal direction
         body.velocity = new Vector2(horizontal_input * speed, body.velocity.y);
-        
-
-        //This changes the direction the player is facing based of horizontal movement
-        if (horizontal_input > 0.01f) {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (horizontal_input < -0.01f) {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        //This is the code for the jumping mechanic
-        if(Input.GetKey(KeyCode.W)) {
-            if (jump_amount < max_jumps) {
-                Jump();
-            }        
-        }
-        else if(Input.GetKey(KeyCode.UpArrow)) {
-            if (jump_amount < max_jumps) {
-                Jump();
-            }
-        }
-        
         
         //Set the animation parameters
         anim.SetBool("running", horizontal_input != 0);
-        anim.SetBool("grounded", grounded);
-    }
+        anim.SetBool("grounded", is_grounded());
 
-    
-    private void Jump() {
-        //This add jumping into the game when w or the up arrow are pressed
-        body.velocity = new Vector2(body.velocity.x, jump_power);
-        anim.SetTrigger("jump");
-        grounded = false;
-        jump_amount = jump_amount + 1;
+        //sets variables if the character is grounded
+        if(is_grounded())
+        {
+            extra_jumps = extra_jumps_count;
+        }
+        
+        //This changes the direction the player is facing based of horizontal movement
+        if(horizontal_input > 0.01f) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(horizontal_input < -0.01f) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) {        //when the player is touching the ground, this is true
-        if (collision.gameObject.tag == "ground") {
-            grounded = true;
-            jump_amount = 0;        //resets jump_amount to 1 when the player is grounded
-
+        //Makes the player jump whenever the space key is pressed once
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Jump();
         }
     }
-    
-    
-// GRAVITY ORIGINALLY SET TO 7
 
+    //how the jump functions works
+    private void Jump() 
+    {
+        if(extra_jumps > 0)
+        {
+            body.velocity = new Vector2(body.velocity.x, jump_power);
+            anim.SetTrigger("jump");
+            extra_jumps--;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {        
+    }
     
+    //checks whether the player is grounded and returns a boolean
+    private bool is_grounded() 
+    {
+        RaycastHit2D ray_cast_hit = Physics2D.BoxCast(box_collider.bounds.center, box_collider.bounds.size, 0, Vector2.down, 0.1f, ground_layer);
+        return ray_cast_hit.collider != null;
+    }
 }
